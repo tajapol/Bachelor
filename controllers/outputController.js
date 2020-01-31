22234ad7c332c51f7527954135efeff72292144e
tajapol <tanjapolz@gmx.de>
2020-01-31 12:06:55 +0100

    output überarbeitet

--- controllers/outputController.js
+++ controllers/outputController.js
@@ -1,12 +1,25 @@
 const Analyze = require("../models/analyzeModel");
-const fs = require("fs");
+// const fs = require("fs");
 
 exports.postOutputPage = (req, res, next) => {
+  // const formatedInput = fs.readFileSync(req.file.path, "utf8");
+
   const ana = new Analyze();
   const result = ana.doAnalyze();
+
+  const directInput = req.file;
 
   req.session.destroy();
   // deleteFile(req);
 
-  res.render("index", { pageTitle: "Output", color: result[0], fonts: result[1] });
+  res.render("index", { pageTitle: "Output", output: true, input: directInput, color: result[0], fonts: result[1] });
 };
+
+// deleteFile = req => {
+//   fs.unlinkSync(req.file.path, err => {
+//     if (err) {
+//       throw new Error(err);
+//       return;
+//     }
+//   });
+// };
